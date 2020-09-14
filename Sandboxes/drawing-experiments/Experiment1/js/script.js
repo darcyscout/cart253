98af9c8296bf3f94fb81e60a2af82dde599a0b40
darcyscout <70165647+darcyscout@users.noreply.github.com>
2020-09-14 12:36:01 -0400

    I didn't experiment

--- Sandboxes/drawing-experiments/Experiment1/js/script.js
+++ Sandboxes/drawing-experiments/Experiment1/js/script.js
@@ -9,7 +9,15 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
-background(35,50,60);
+  createCanvas(500,500);
+  background(255,200,200);
+  ellipseMode(CENTER);
+  ellipse(250,250,200,200);
+  fill(0);
+  ellipse(200,225,20,20);
+  ellipse(300,225,20,20);
+  strokeWeight(10);
+  line(200,275,300,275);
 }
 
 // draw()
