de8e2cc132c1704e437fd4ac7954720375e2ac7f
darcyscout <70165647+darcyscout@users.noreply.github.com>
2020-09-28 15:12:49 -0400

    Ex1: Circles change colour
    I haven't added comments yet

--- Exercises/exercise1/js/script.js
+++ Exercises/exercise1/js/script.js
@@ -8,6 +8,8 @@ Here is a description of this template p5 project.
 // setup()
 //
 // Description of setup() goes here.
+/* Setting up 4 circle objects and a background */
+
 let bg = {
   r: 0,
   g: 0,
@@ -27,7 +29,9 @@ let circle2 = {
   x: 500,
   y: 250,
   size: 80,
-  fill: 220,
+  r: 0,
+  g: 255,
+  b: 0,
   alpha: 200,
   speed: 0.157
 };
@@ -38,7 +42,7 @@ let circle3 = {
   size: 60,
   r: 0,
   g: 0,
-  b: 0,
+  b: 255,
   alpha: 200,
   speed: 0.157
 };
@@ -67,25 +71,35 @@ circle1.x = circle1.x + circle1.speed; // Move the circle right
 circle1.x = constrain(circle1.x, 0, width/2);
 circle1.size = circle1.size + 0.25;
 circle1.size = constrain(circle1.size, 0, width);
-  // Circle 2
+circle1.fill = circle1.fill - 0.18;
+circle1.fill = constrain(circle1.fill, 0, 255);
 
-  fill(circle2.fill, circle2.fill, circle2.fill, circle2.alpha);
+  // Circle 2
+  fill(circle2.r, circle2.g, circle2.b, circle2.alpha);
   ellipse(circle2.x,circle2.y,circle1.size*0.8,circle1.size*0.8);
   circle2.x = circle2.x - circle2.speed; // Move the circle left
   circle2.x = constrain(circle2.x, width/2, width);
+  circle2.b = circle2.b + 0.18;
+  circle2.b = constrain(circle2.b, 0, 255);
+  circle2.g = circle2.g - 0.18;
+  circle2.g = constrain(circle2.g, 0, 255);
 
+  // Circle 3
   fill(circle3.r, circle3.g, circle3.b, circle3.alpha);
   ellipse(circle3.x,circle3.y,circle1.size*0.6,circle1.size*0.6);
   circle3.y = circle3.y + circle3.speed; // Move the circle down
   circle3.y = constrain(circle3.y, 0, height/2);
+  circle3.r = circle3.r + 0.18;
+  circle3.r = constrain(circle3.r, 0, 255);
+  circle3.b = circle3.b - 0.18;
+  circle3.b = constrain(circle3.b, 0, 255);
 
+  // Circle 4
   fill(circle4.fill, circle4.fill, circle4.fill, circle4.alpha);
   ellipse(circle4.x,circle4.y,circle1.size*0.4,circle1.size*0.4);
   circle4.y = circle4.y - circle4.speed; // Move the circle down
   circle4.y = constrain(circle4.y, height/2, height);
   circle4.fill = circle4.fill + 0.07;
   circle4.fill = constrain(circle4.fill, 0, 255);
-
-  console.log(circle4.fill);
 
 }
