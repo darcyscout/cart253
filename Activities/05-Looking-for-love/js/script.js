2ac95efb5a55ecd45cd78ce5f1f2a41292495e7f
darcyscout <70165647+darcyscout@users.noreply.github.com>
2020-10-04 22:41:59 -0400

    Act 5: Starting to make States
    title, simulation, love, sadness

--- Activities/05-Looking-for-love/js/script.js
+++ Activities/05-Looking-for-love/js/script.js
@@ -23,11 +23,15 @@ let circle2 = {
   vy: 0
 }
 
+let state = 'simulation'; //Can be: title. simulation, love, sadness
+
 function setup() {
   createCanvas(500, 500);
+}
+
+function setupCircles() {
   circle1.x = width/3;
   circle2.x = width/3 * 2;
-
   circle1.vx = random(circle1.speed,-circle1.speed);
   circle1.vy = random(circle1.speed,-circle1.speed);
   circle2.vx = random(circle2.speed,-circle2.speed);
@@ -36,17 +40,52 @@ function setup() {
 
 function draw() {
   background(0);
-  ellipse(circle1.x,circle1.y,circle1.size,circle1.size);
-  ellipse(circle2.x,circle2.y,circle2.size,circle2.size);
 
+  if (state === 'title') {
+
+  }
+  else if (state === 'simulation'){
+      simulation();
+  }
+  else if (state === 'love'){
+
+  }
+  else if (state === 'sadness'){
+
+  }
+
+}
+
+function simulation() {
+  move();
+  checkOffScreen();
+  checkOverlap();
+  display();
+}
+
+function move() {
   circle1.x = circle1.x + circle1.vx;
   circle1.y = circle1.y + circle1.vy;
   circle2.x = circle2.x + circle2.vx;
   circle2.y = circle2.y + circle2.vy;
-
+}
+function checkOffScreen() {
   if (circle1.x >= width || circle1.x <= 0 || circle1.y >= height|| circle1.y <= 0
     || circle2.x >= width || circle2.x <= 0 || circle2.y >= height
     || circle2.y <= 0) {
     background(100);   // Sad Ending
   }
+}
+
+function checkOverlap() {
+let d = dist(circle1.x,circle1.y,circle2.x,circle2.y);
+  if (d <= 100) {
+    background(170, 0, 0); // Happy Ending
+  }
+}
+
+function display() {
+  noStroke();
+  ellipse(circle1.x,circle1.y,circle1.size,circle1.size);
+  ellipse(circle2.x,circle2.y,circle2.size,circle2.size);
 }
