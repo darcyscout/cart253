e39af1955c2e055d9142c8857c6897ef2662777a
darcyscout <70165647+darcyscout@users.noreply.github.com>
2020-10-11 03:15:59 -0400

    Proj1: Redoing background later
    For now I will make the central piece and make the background i want at the end

--- Projects/project1/js/script.js
+++ Projects/project1/js/script.js
@@ -8,30 +8,55 @@ Here is a description of this template p5 project
 // setup()
 //
 // Description of setup() goes here.
+let middleObject = {
+  x: 0,
+  y: 0,
+  size: 100,
+  rotation: 0
+}
 
 let dot = {
   x: 0,
   y: 0,
-  alpha: 255
+  alpha: 255,
+  dotsToDraw: 10000,
+  dotsDrawn: 0
 }
 
 function setup() {
-  createCanvas(windowWidth * 0.999,windowHeight * 0.992);  //The canvas sometimes was too big for the browser and reacted poorly
+  rectMode(CENTER);
+  angleMode(DEGREES);
+  createCanvas(windowWidth * 0.999,windowHeight * 0.992);  //The canvas sometimes was too big for the browser and had scroll bars
   frameRate(60);
 }
 
 
 
 function draw() {
-  background(0, 0, 0,);
+  background(0, 0, 0, 25);
+  // while (dot.dotsDrawn < dot.dotsToDraw) {
+  //   dot.x = random(0,width);
+  //   dot.y = random(0,height);
+  //   dot.alpha = random(0,255);
+  //   stroke(255,255,255,dot.alpha);
+  //   point(dot.x,dot.y);
+  //   dot.dotsDrawn = dot.dotsDrawn + 1;
+  // }
 
-  for (let i = 0; i < 10000; i++) {
+  for (let i = 0; i < 1000; i++) {
     dot.x = random(0,width);
     dot.y = random(0,height);
     dot.alpha = random(0,255);
     stroke(255,255,255,dot.alpha);
     point(dot.x,dot.y);
-    noLoop();
   }
-  ellipse(width/2,height/2,100,100);
+  // push();
+  // noStroke();
+  // fill(255,255,255,255);
+  // translate(width/2,height/2);
+  // rotate(middleObject.rotation);
+  // rect(middleObject.x,middleObject.y,middleObject.size,middleObject.size);
+  // pop();
+  //
+  // middleObject.rotation = middleObject.rotation + 1;
 }
