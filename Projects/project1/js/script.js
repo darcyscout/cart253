9468cb64c3699a85b879262831a1a3b1c625fdcf
darcyscout <70165647+darcyscout@users.noreply.github.com>
2020-10-12 20:39:12 -0400

    Proj1: Putting Everything into functions

--- Projects/project1/js/script.js
+++ Projects/project1/js/script.js
@@ -15,6 +15,11 @@ let middleObject = {
   rotation: 0
 }
 
+let openingText = {
+  alpha: 0,
+  alphaChange: 0,
+}
+
 let bg = {
   fill: 0,
   alpha: 25,
@@ -39,6 +44,17 @@ let circle2 = {
   eclipseSpeed: 0.2
 }
 
+let circle3 = {
+  x: 0,
+  y: 0,
+  size: 10,
+  alpha: 0,
+  alphaChange: 0,
+  r: 255,
+  g: 255,
+  b: 255,
+}
+
 let dot = {
   x: 0,
   y: 0,
@@ -53,32 +69,31 @@ let dot = {
   dotWeightChange: 0,
 }
 
+let state = `title`
+
 function setup() {
   rectMode(CENTER);
   angleMode(DEGREES);
   createCanvas(windowWidth * 0.999,windowHeight * 0.992);  //The canvas sometimes was too big for the browser and had scroll bars
   frameRate(60);
   background(0);   //Background starts absolute black so the transparent background in draw doesn't look grey
+
+  function setupTitleCircles(){
   circle1.x = width/2;
   circle1.y = height/2;
   circle2.x = width/2;
   circle2.y = height/2;
+  circle3.x = width/2;
+  circle3.y = height/2;
+}
 }
 
 
 
 function draw() {
 
+function titleBackgrounnd() {
   background(bg.fill, bg.fill, bg.fill, bg.alpha);
-  // while (dot.dotsDrawn < dot.dotsToDraw) {
-  //   dot.x = random(0,width);
-  //   dot.y = random(0,height);
-  //   dot.alpha = random(0,255);
-  //   stroke(255,255,255,dot.alpha);
-  //   point(dot.x,dot.y);
-  //   dot.dotsDrawn = dot.dotsDrawn + 1;
-  // }
-
   for (let i = 0; i < dot.dotsDrawn; i++) {
     dot.x = random(0,width);
     dot.y = random(0,height);
@@ -87,46 +102,60 @@ function draw() {
     stroke(dot.r,dot.g,dot.b,dot.alpha);
     point(dot.x,dot.y);
   }
-  noStroke();
-  fill(circle1.r,circle1.g,circle1.b);
-  ellipse(circle1.x,circle1.y,circle1.size,circle1.size);
+}
 
+function titleVariableChanges() {
+  openingText.alpha = openingText.alpha + openingText.alphaChange;
   dot.g = dot.g + dot.colourSpeed;
   dot.b = dot.b + dot.colourSpeed;
-  // dot.dotWeight = dot.dotWeight + dot.dotWeightChange;
-  // dot.dotsDrawn = dot.dotsDrawn + dot.dotsDrawnChange;
   circle1.g = circle1.g + circle1.colourSpeed;
   circle1.b = circle1.b + circle1.colourSpeed;
   circle1.size = circle1.size + circle1.sizeChange
+  circle2.eclipseDist = circle2.eclipseDist + circle2.eclipseSpeed;
+  circle3.alpha = circle3.alpha + circle3.alphaChange;
+}
 
+function displayTitleCircles() {
+  noStroke();
+  fill(circle1.r,circle1.g,circle1.b);
+  ellipse(circle1.x,circle1.y,circle1.size,circle1.size);
   fill(0);
   ellipse(circle2.x + circle2.eclipseDist,circle2.y,circle2.size,circle2.size);
-  circle2.eclipseDist = circle2.eclipseDist + circle2.eclipseSpeed;
+  fill(circle3.r,circle3.g,circle3.b,circle3.alpha);
+  ellipse(circle3.x,circle3.y,circle3.size,circle3.size);
+}
 
-    // if (circle2.eclipseDist >= 105 || circle2.eclipseDist <= -105) {
-    //   circle2.eclipseSpeed = -circle2.eclipseSpeed;
-    // }
-
+function titleIfStatements() {
     if (circle2.eclipseDist >= 0) {
       circle2.eclipseSpeed = 0;
       circle1.colourSpeed = -1;
       dot.colourSpeed = -1;
     }
-
     if (circle1.g <= 0) {
       circle1.sizeChange = 1;
       // dot.dotWeightChange = 0.1;
       // dot.dotsDrawnChange = -1;
       dot.alpha = 255;
-      // bg.alpha = 255;
     }
-  // push();
-  // noStroke();
-  // fill(255,255,255,255);
-  // translate(width/2,height/2);
-  // rotate(middleObject.rotation);
-  // rect(middleObject.x,middleObject.y,middleObject.size,middleObject.size);
-  // pop();
-  //
-  // middleObject.rotation = middleObject.rotation + 1;
+    if (circle1.size > 500) {     // Opening Text
+      push();
+      textSize(64);
+      fill(0,0,0,openingText.alpha);    // Black Text
+      textAlign(CENTER,CENTER);
+      text(`WELCOME`,width/2,height/2 - 100);   //  WELCOME
+      pop();
+      openingText.alphaChange = 10;
+      circle3.alphaChange = 10;
+    }
 }
+
+} // Draw End
+
+  function mousePressed() {
+    let d = dist(mouseX,mouseY,circle2.x,circle2.y)
+    if (d < 100) {
+      if (circle3.alphaChange >= 10) {
+              circle3.r = 0;
+      }
+    }
+  } // MousePressedEnd
