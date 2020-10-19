33cf3d410eb0c50d628a290fa1a13cfb95200624
darcyscout <70165647+darcyscout@users.noreply.github.com>
2020-10-19 18:19:18 -0400

    sandbox: array practice

---
+++ Sandboxes/Arrays/js/script.js
@@ -0,0 +1,76 @@
+"use strict";
+
+let school = [];
+let schoolSize = 100;
+
+function setup() {
+  createCanvas(600, 600);
+
+  for (let i = 0; i < schoolSize; i++) {
+    // Create a fish
+    let fish = createFish(random(0, width), random(0, height));
+    // Add the fish to our array
+    school.push(fish);
+  }
+}
+function setup() {
+  createCanvas(600,600);
+
+  for (let i = 0; i < schoolSize; i++) {
+    school[i] = createFish(random(0,width),random(0,height));
+  }
+}
+
+// createFish(x,y)
+// Creates a new JavaScript Object describing a fish and returns it
+function createFish(x, y) {
+  let fish = {
+    x: x,
+    y: y,
+    size: 50,
+    vx: 0,
+    vy: 0,
+    speed: 2
+  };
+  return fish;
+}
+
+// draw()
+// Moves and displays our fish
+function draw() {
+  background(0);
+
+  for (let i = 0; i < school.length; i++) {
+    moveFish(school[i]);
+    displayFish(school[i]);
+  }
+}
+
+// moveFish(fish)
+// Chooses whether the provided fish changes direction and moves it
+function moveFish(fish) {
+  // Choose whether to change direction
+  let change = random(0, 1);
+  if (change < 0.05) {
+    fish.vx = random(-fish.speed, fish.speed);
+    fish.vy = random(-fish.speed, fish.speed);
+  }
+
+  // Move the fish
+  fish.x = fish.x + fish.vx;
+  fish.y = fish.y + fish.vy;
+
+  // Constrain the fish to the canvas
+  fish.x = constrain(fish.x, 0, width);
+  fish.y = constrain(fish.y, 0, height);
+}
+
+// displayFish(fish)
+// Displays the provided fish on the canvas
+function displayFish(fish) {
+  push();
+  fill(200, 100, 100);
+  noStroke();
+  ellipse(fish.x, fish.y, fish.size);
+  pop();
+}
