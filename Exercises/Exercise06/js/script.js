a3a3530b680360fab280e4c6a2cc2ed55940c1ce
darcyscout <70165647+darcyscout@users.noreply.github.com>
2020-11-10 18:13:18 -0500

    Ex6: Finished
    I think this is what we were supposed to do for the exercise but I am not entirely sure.

--- Exercises/Exercise06/js/script.js
+++ Exercises/Exercise06/js/script.js
@@ -1,52 +1,31 @@
 "use strict";
 
-let barkSFX;
-let peaks; // To store our array of peaks
-
-function preload() {
-  barkSFX = loadSound(`assets/sounds/bark.wav`);
-}
+let oscillator;
+let oscillator2;
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(windowWidth, windowHeight);
   userStartAudio();
 
-  // Use getPeaks() to get the peaks in our sound file
-  // We specify "width" as the size of the array of peaks
-  // so that we can easily draw the waveform across the
-  // width of the canvas, one piece of peak data per pixel
-  peaks = barkSFX.getPeaks(width);
+  oscillator = new p5.Oscillator(440, `square`);
+  oscillator.amp(0.13);
+
+  oscillator2 = new p5.Oscillator(440, `sawtooth`);
+  oscillator2.amp(0.16);
 }
 
 function draw() {
   background(0);
+  ellipse(width/2,height/2,mouseX,mouseY);
 
-  // Display the peaks if the sound file is currently playing
-  if (barkSFX.isPlaying()) {
-    drawPeaks();
-  }
-}
+  let newFreq = map(mouseY, height, 0, 0, 440);
+  oscillator.freq(newFreq);
 
-// drawPeaks() runs through the array of peaks and graphs them
-function drawPeaks() {
-  push();
-  stroke(255);
-  // Run through every peak in the array
-  for (let i = 0; i < peaks.length; i++) {
-    // Get the current peak data
-    let peak = peaks[i];
-    // Map the data to a y position. The peak data is between -1 and 1
-    // but we want to display it on the canvas, so we map to a number
-    // between 0 and height
-    let y = map(peak, -1, 1, 0, height);
-    // Draw a line from the center of the canvas to the mapped peak value
-    // with an x set to "i" because we're going through an array the
-    // width of the canvas...
-    line(i, height / 2, i, y);
-  }
-  pop();
+  let newFreq2 = map(mouseX, width, 0, 0, 440);
+  oscillator2.freq(newFreq2);
 }
 
 function mousePressed() {
-  barkSFX.play();
+  oscillator.start();
+  oscillator2.start();
 }
